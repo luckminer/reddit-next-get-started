4bdf63ece345d0716f32d786f1704a9e2d494902
luckminer <bloodyblades325@outlook.com>
2022-02-07 17:54:19 +0100

    refactoring w next-urql

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,8 +8,11 @@ const Index = () => {
   return (
     <>
       <NavBar />
-      <p>Hello World</p>
-      {!data ? null : data.posts.map((p) => <div key={p.id}>{p.title}</div>)}
+      {!data ? (
+        <div>Loading...</div>
+      ) : (
+        data.posts.map((p) => <div key={p.id}>{p.title}</div>)
+      )}
     </>
   );
 };
